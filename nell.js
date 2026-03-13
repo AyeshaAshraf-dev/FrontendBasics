fe893c1d2de857fcc6450a731697c21dc0b72591
Ayesha Ashraf <234213813+AyeshaAshraf-dev@users.noreply.github.com>
2026-03-13 16:02:48 +0500

    again basics

---
+++ nell.js
@@ -0,0 +1,62 @@
+
+// 4. Loops
+
+// Print 1 to 50
+// let i;
+// for(i = 0;i<=50;i++) {
+//     console.log("i : ",i,"\n");
+    // i++;
+// }
+// Print all even numbers from 1-100
+// let y;
+// for(y=2;y<=100;y++){
+//     // if(y%2==0){ 
+//     console.log("\nEven numbers: ",y);
+//     y++;
+//     // }
+// }
+// Sum of first 100 numbers
+
+// let i=0;
+// let sum = 0;
+// while (i<=100) {
+//     sum += i;
+//     i++;
+// }
+// console.log("sum: ",sum)              
+
+// Multiplicatiogn table of any number
+// let num7 = 4;
+// let h;
+// for(h=1;h<=10;h++) {
+//     lest = num7*h
+//     console.log(`${num7}*${h}=${lest}`)
+
+// }
+// Reverse a number
+// console.log("sds")
+// let number= 4352;
+// let reversed = 0;
+// let newl;
+// while(number>0) {
+//     newl = number%10
+//     number = Math.floor(number/10)
+//     reversed = reversed*10+ newl
+// }
+//     console.log(reversed)
+// Count digits in a number
+// let number = 3432434;
+// let count =0;
+// while(number>0) {
+//     number = Math.floor(number/10)
+//     count++;
+
+// }
+// console.log(count)
+
+
+// Print fibonacci sequence
+// Find factorial
+// Print pattern of stars (triangle)
+// Guess the number game (loop until correct)
+
